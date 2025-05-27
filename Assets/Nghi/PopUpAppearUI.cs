68fc5dd345678320b2eeff79b041c1fdd677bfd7
TranVuPhuongNghi <169137983+TranVuPhuongNghi@users.noreply.github.com>
2025-05-27 22:29:52 +0700

    Color + Effect

---
+++ Assets/Nghi/PopUpAppearUI.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpAppearUI : MonoBehaviour
+{
+    [System.Serializable]
+    public class AppearItem
+    {
+        public GameObject target;
+        public float delay = 0.2f;
+        public AnimationType animation = AnimationType.Scale;
+        public float duration = 0.3f;
+        public Vector3 fromPositionOffset = new Vector3(0, -50f, 0);
+        public float fromAlpha = 0f;
+    }
+
+    public enum AnimationType
+    {
+        Scale,
+        MoveFromOffset,
+        FadeIn,
+        ScaleAndFade,
+        MoveAndFade
+    }
+
+    [Header("Config")]
+    public List<AppearItem> appearSequence = new List<AppearItem>();
+    public bool playOnStart = true;
+
+    void Start()
+    {
+        if (playOnStart)
+        {
+            HideAllItems();
+            StartSequence();
+        }
+    }
+
+    public void StartSequence()
+    {
+        StartCoroutine(PlaySequence());
+    }
+
+    IEnumerator PlaySequence()
+    {
+        foreach (var item in appearSequence)
+        {
+            if (item.target == null) continue;
+
+            PrepareItem(item);
+
+            yield return new WaitForSeconds(item.delay);
+
+            PlayAnimation(item);
+            yield return new WaitForSeconds(item.duration);
+        }
+    }
+
+    void PrepareItem(AppearItem item)
+    {
+        var t = item.target.transform;
+
+        // Setup ban đầu tuỳ theo Animation
+        switch (item.animation)
+        {
+            case AnimationType.Scale:
+                t.localScale = Vector3.zero;
+                break;
+            case AnimationType.MoveFromOffset:
+                t.localPosition += item.fromPositionOffset;
+                break;
+            case AnimationType.FadeIn:
+                SetAlpha(item.target, item.fromAlpha);
+                break;
+            case AnimationType.ScaleAndFade:
+                t.localScale = Vector3.zero;
+                SetAlpha(item.target, item.fromAlpha);
+                break;
+            case AnimationType.MoveAndFade:
+                t.localPosition += item.fromPositionOffset;
+                SetAlpha(item.target, item.fromAlpha);
+                break;
+        }
+
+        item.target.SetActive(true);
+    }
+
+    void PlayAnimation(AppearItem item)
+    {
+        var t = item.target.transform;
+
+        switch (item.animation)
+        {
+            case AnimationType.Scale:
+                LeanTween.scale(t.gameObject, Vector3.one, item.duration).setEaseOutBack();
+                break;
+            case AnimationType.MoveFromOffset:
+                LeanTween.moveLocal(t.gameObject, t.localPosition - item.fromPositionOffset, item.duration).setEaseOutCubic();
+                break;
+            case AnimationType.FadeIn:
+                LeanTween.value(item.target, item.fromAlpha, 1f, item.duration)
+                    .setOnUpdate((float val) => SetAlpha(item.target, val));
+                break;
+            case AnimationType.ScaleAndFade:
+                LeanTween.scale(t.gameObject, Vector3.one, item.duration).setEaseOutBack();
+                LeanTween.value(item.target, item.fromAlpha, 1f, item.duration)
+                    .setOnUpdate((float val) => SetAlpha(item.target, val));
+                break;
+            case AnimationType.MoveAndFade:
+                LeanTween.moveLocal(t.gameObject, t.localPosition - item.fromPositionOffset, item.duration).setEaseOutCubic();
+                LeanTween.value(item.target, item.fromAlpha, 1f, item.duration)
+                    .setOnUpdate((float val) => SetAlpha(item.target, val));
+                break;
+        }
+    }
+
+    void SetAlpha(GameObject obj, float alpha)
+    {
+        var canvasGroup = obj.GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = obj.AddComponent<CanvasGroup>();
+        }
+
+        canvasGroup.alpha = alpha;
+    }
+
+    void HideAllItems()
+    {
+        foreach (var item in appearSequence)
+        {
+            if (item.target != null)
+            {
+                item.target.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Nghi/UIPopupAnimator.cs
@@ -0,0 +1,162 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPopupAnimator : MonoBehaviour
+{
+    [System.Serializable]
+    public class FadeItem
+    {
+        public GameObject target;
+        public float delay = 0f;
+        public float duration = 0.5f;
+        public Ease ease = Ease.OutExpo;
+        public EffectType effectType = EffectType.Center;
+    }
+
+    [System.Serializable]
+    public class TMPFadeItem
+    {
+        public TextMeshProUGUI targetText;
+        public float delay = 0.1f;
+        public float duration = 0.4f;
+        public Ease ease = Ease.OutExpo;
+        public EffectType effectType = EffectType.Center;
+    }
+
+    public enum EffectType
+    {
+        Center,
+        LeftToRight,
+        RightToLeft
+    }
+
+    [Header("Global Settings")]
+    public bool playOnStart = true;
+    public bool hideAtStart = true;
+
+    [Header("UI GameObjects Fade List")]
+    public List<FadeItem> fadeItems = new List<FadeItem>();
+
+    [Header("TMP Text Fade List")]
+    public List<TMPFadeItem> tmpFadeItems = new List<TMPFadeItem>();
+
+    void Start()
+    {
+        if (hideAtStart)
+        {
+            foreach (var item in fadeItems)
+            {
+                if (item.target == null) continue;
+
+                CanvasGroup cg = GetOrAddCanvasGroup(item.target);
+                cg.alpha = 0f;
+
+                RectTransform rt = item.target.GetComponent<RectTransform>();
+                rt.localScale = new Vector3(0f, 1f, 1f);
+            }
+
+            foreach (var item in tmpFadeItems)
+            {
+                if (item.targetText == null) continue;
+
+                CanvasGroup cg = GetOrAddCanvasGroup(item.targetText.gameObject);
+                cg.alpha = 0f;
+
+                RectTransform rt = item.targetText.rectTransform;
+                rt.localScale = new Vector3(0f, 1f, 1f);
+            }
+        }
+
+        if (playOnStart)
+        {
+            PlayFadeSequence();
+        }
+    }
+
+    public void PlayFadeSequence()
+    {
+        // GameObject UI elements
+        foreach (var item in fadeItems)
+        {
+            if (item.target == null) continue;
+
+            CanvasGroup cg = GetOrAddCanvasGroup(item.target);
+            RectTransform rt = item.target.GetComponent<RectTransform>();
+
+            Vector2 originalPos = rt.anchoredPosition;
+            Vector2 offsetPos = originalPos;
+            float width = rt.rect.width;
+
+            switch (item.effectType)
+            {
+                case EffectType.LeftToRight:
+                    offsetPos = new Vector2(originalPos.x - width * 0.5f, originalPos.y);
+                    rt.anchoredPosition = offsetPos;
+                    break;
+                case EffectType.RightToLeft:
+                    offsetPos = new Vector2(originalPos.x + width * 0.5f, originalPos.y);
+                    rt.anchoredPosition = offsetPos;
+                    break;
+            }
+
+            cg.alpha = 0f;
+            rt.localScale = new Vector3(0f, 1f, 1f);
+
+            cg.DOFade(1f, item.duration).SetEase(item.ease).SetDelay(item.delay);
+            rt.DOScaleX(1f, item.duration).SetEase(item.ease).SetDelay(item.delay);
+
+            if (item.effectType != EffectType.Center)
+            {
+                rt.DOAnchorPos(originalPos, item.duration).SetEase(item.ease).SetDelay(item.delay);
+            }
+        }
+
+        // TMP elements
+        foreach (var item in tmpFadeItems)
+        {
+            if (item.targetText == null) continue;
+
+            GameObject go = item.targetText.gameObject;
+            CanvasGroup cg = GetOrAddCanvasGroup(go);
+            RectTransform rt = item.targetText.rectTransform;
+
+            Vector2 originalPos = rt.anchoredPosition;
+            Vector2 offsetPos = originalPos;
+            float width = rt.rect.width;
+
+            switch (item.effectType)
+            {
+                case EffectType.LeftToRight:
+                    offsetPos = new Vector2(originalPos.x - width * 0.5f, originalPos.y);
+                    rt.anchoredPosition = offsetPos;
+                    break;
+                case EffectType.RightToLeft:
+                    offsetPos = new Vector2(originalPos.x + width * 0.5f, originalPos.y);
+                    rt.anchoredPosition = offsetPos;
+                    break;
+            }
+
+            cg.alpha = 0f;
+            rt.localScale = new Vector3(0f, 1f, 1f);
+
+            cg.DOFade(1f, item.duration).SetEase(item.ease).SetDelay(item.delay);
+            rt.DOScaleX(1f, item.duration).SetEase(item.ease).SetDelay(item.delay);
+
+            if (item.effectType != EffectType.Center)
+            {
+                rt.DOAnchorPos(originalPos, item.duration).SetEase(item.ease).SetDelay(item.delay);
+            }
+        }
+    }
+
+    private CanvasGroup GetOrAddCanvasGroup(GameObject go)
+    {
+        CanvasGroup cg = go.GetComponent<CanvasGroup>();
+        if (cg == null) cg = go.AddComponent<CanvasGroup>();
+        return cg;
+    }
+}
