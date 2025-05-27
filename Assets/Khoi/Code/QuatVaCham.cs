999b4db3f162ba78d9bee51753e36e280766e493
khoyyy <169151587+khoyyy@users.noreply.github.com>
2025-05-27 18:22:37 +0700

    new

---
+++ Assets/Khoi/Code/QuatVaCham.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class QuatVaCham : MonoBehaviour
+{
+    // Khi va chạm trigger với player
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Debug để kiểm tra
+            Debug.Log("Player bị trúng cánh quạt!");
+
+            // Bạn có thể gọi hàm chết ở player nếu có script Player
+            // other.GetComponent<Player>().Die();  // nếu có
+
+            // Hoặc đơn giản là phá hủy player
+            Destroy(other.gameObject);
+        }
+    }
+}
