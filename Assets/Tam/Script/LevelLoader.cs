0eab54e085cc102eb9724cc0953cf72c4f9177a6
Tam Doan <118838652+tamhoccodegame@users.noreply.github.com>
2025-06-23 18:29:38 +0700

    Level Loader Script

--- Assets/Tam/Script/LevelLoader.cs
+++ Assets/Tam/Script/LevelLoader.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static LevelLoader instance;
+
+    public Animator animator;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartLoad()
     {
-        
+        animator.Play("StartLoad");
     }
+
 }
