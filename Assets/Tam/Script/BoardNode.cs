b6f60e1627d2c13d9d4d32351402a2902cbc03b6
Tam Doan <118838652+tamhoccodegame@users.noreply.github.com>
2025-06-25 17:20:41 +0700

    Afsa

--- Assets/Tam/Script/BoardNode.cs
+++ Assets/Tam/Script/BoardNode.cs
@@ -38,7 +38,7 @@ public class BoardNode : NetworkBehaviour
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     public void RPC_ProcessNode(PlayerRef player)
     {
-        if(eventType == EventType.None)
+        //if(eventType == EventType.None)
         //nodeEffect.Play();
         //else
         {
