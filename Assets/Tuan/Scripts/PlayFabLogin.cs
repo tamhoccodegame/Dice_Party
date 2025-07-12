e4b214d7b4afb1735297e6070cf4a57b5d0dd64f
OopsShennen <95273055+OopsShennen@users.noreply.github.com>
2025-07-12 13:58:30 +0700

    a

---
+++ Assets/Tuan/Scripts/PlayFabLogin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PlayFab.ClientModels;
+using PlayFab;
+using TMPro;
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+{
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
+
+    public void OnLoginButtonClick()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+    }
+
+    void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Đăng nhập thành công: " + result.PlayFabId);
+    }
+
+    void OnLoginFailure(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
