8c007d88156e570d3d0f7a6f03c1233f37597537
OopsShennen <95273055+OopsShennen@users.noreply.github.com>
2025-07-16 15:20:01 +0700

    a

--- Assets/Tuan/Scripts/PlayFabEconomy.cs
+++ Assets/Tuan/Scripts/PlayFabEconomy.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using PlayFab.ClientModels;
+using PlayFab.EconomyModels;
 using PlayFab;
 using UnityEngine;
 
-public class PlayFabEconomy : MonoBehaviour
+public class ItemsPurchase : MonoBehaviour
 {
     public void RefreshInventory()
     {
@@ -30,35 +30,88 @@ public class PlayFabEconomy : MonoBehaviour
             res => Debug.Log($"+{amount} Coins (new balance {res.Balance})"),
             err => Debug.LogError(err.GenerateErrorReport()));
     }
-    public void SpendCoins(int amount)
+
+   /* public void BuySkin(string skinItemId)
     {
-        var req = new SubtractUserVirtualCurrencyRequest
+        var request = new SubmitInventoryPurchaseRequest
         {
-            VirtualCurrency = "CO",
-            Amount = amount
+            Item = new InventoryItemReference
+            {
+                Id = "SKIN_NINJA",
+                Type = "catalogItem"
+            },
+            PriceAmounts = new List<InventoryPriceAmount>
+    {
+        new InventoryPriceAmount
+        {
+            Amount = 500,
+            Item = new InventoryItemReference
+            {
+                Id = "CO",
+                Type = "currency"
+            }
+        }
+    }
         };
 
-        PlayFabClientAPI.SubtractUserVirtualCurrency(req,
-            res => Debug.Log($"-{amount} Coins (bal. {res.Balance})"),
+        PlayFabEconomyAPI.SubmitInventoryPurchase(request,
+            res => {
+                Debug.Log("Mua skin Economy V2 thành công!");
+            },
             err => Debug.LogError(err.GenerateErrorReport()));
-    }
-    public void GrantSword()
+    }*/
+/*    public void ShowOwnedSkins()
     {
-        PlayFabClientAPI.ExecuteCloudScript(new ExecuteCloudScriptRequest
-        {
-            FunctionName = "Main",
-            FunctionParameter = new { itemId = "Sword" }
-        },
-        res => Debug.Log("Đã nhận Sword"),
-        err => Debug.LogError(err.GenerateErrorReport()));
-    }
-    public void SaveEquipped(string weaponId)
+        PlayFabClientAPI.GetUserInventory(new GetUserInventoryRequest(),
+            res => {
+                foreach (var item in res.Inventory)
+                {
+                    if (item.ItemClass == "skin")
+                        Debug.Log($"Sở hữu skin: {item.DisplayName} ({item.ItemId})");
+                }
+            },
+            err => Debug.LogError(err.GenerateErrorReport()));
+    }*/
+/*    public void SetEquippedSkin(string itemId)
     {
-        PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest
+        var req = new UpdateUserDataRequest
         {
-            Data = new Dictionary<string, string> { { "equippedWeapon", weaponId } }
-        },
-        _ => Debug.Log("Đã lưu weapon"),
-        err => Debug.LogError(err.GenerateErrorReport()));
-    }
+            Data = new Dictionary<string, string> {
+            { "EquippedSkin", itemId }
+        }
+        };
+        PlayFabClientAPI.UpdateUserData(req,
+            res => Debug.Log("Trang phục đã chọn: " + itemId),
+            err => Debug.LogError(err.GenerateErrorReport()));
+    }*/
+
+   /* public void GetEquippedSkin(System.Action<string> callback)
+    {
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest(),
+            res => {
+                if (res.Data.TryGetValue("EquippedSkin", out var entry))
+                    callback(entry.Value);
+                else
+                    callback(null);
+            },
+            err => Debug.LogError(err.GenerateErrorReport()));
+    }*/
+    /*public void TryBuySkin(string skinItemId)
+    {
+        PlayFabClientAPI.GetUserInventory(new GetUserInventoryRequest(),
+            res => {
+                bool hasSkin = res.Inventory.Any(i => i.ItemId == skinItemId);
+                if (hasSkin)
+                {
+                    Debug.Log("Bạn đã sở hữu skin này rồi.");
+                    SetEquippedSkin(skinItemId); // Hoặc hiện nút [Trang bị]
+                }
+                else
+                {
+                    BuySkin(skinItemId); // Gọi mua nếu chưa có
+                }
+            },
+            err => Debug.LogError(err.GenerateErrorReport()));
+    }*/
+
 }
