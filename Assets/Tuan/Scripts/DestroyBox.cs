a3f1af4c0a75e9a44d25ebcd749d9039e7880b85
OopsShennen <95273055+OopsShennen@users.noreply.github.com>
2025-07-20 14:24:33 +0700

    a

---
+++ Assets/Tuan/Scripts/DestroyBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Crate"))
+        {
+            other.gameObject.SetActive(false);
+            StartCoroutine(RespawnCrate(other.gameObject, 5f));
+        }
+    }
+
+    IEnumerator RespawnCrate(GameObject crate, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        crate.SetActive(true);
+    }
+}
