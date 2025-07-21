686de2ae220dec93949633187d3f64d927ebe584
OopsShennen <95273055+OopsShennen@users.noreply.github.com>
2025-07-21 19:45:41 +0700

    a

--- Assets/Tuan/Scripts/DestroyBox.cs
+++ Assets/Tuan/Scripts/DestroyBox.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class DestroyBox : MonoBehaviour
 {
+    public ParticleSystem vfx;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Crate"))
         {
             other.gameObject.SetActive(false);
+            ParticleSystem vfxInstance = Instantiate(vfx, other.transform.position, Quaternion.identity);
+            vfxInstance.Play();
+            Destroy(vfxInstance.gameObject, vfxInstance.main.duration + 0.5f);
             StartCoroutine(RespawnCrate(other.gameObject, 5f));
         }
     }
