2bf33a0bbbb683031b31d230c3c90f8abf5b7618
Tam Doan <118838652+tamhoccodegame@users.noreply.github.com>
2025-08-02 22:27:46 +0700

    Sửa map

--- Assets/Project/Runtime/Scripts/Controller/NewBoardGameController.cs
+++ Assets/Project/Runtime/Scripts/Controller/NewBoardGameController.cs
@@ -172,8 +172,8 @@ public class NewBoardGameController : PlayerController
 
     IEnumerator RollDiceCoroutine()
     {
-        //StepsLeft = Random.Range(1, 9);
-        StepsLeft = 99;
+        StepsLeft = Random.Range(1, 9);
+        //StepsLeft = 99;
 
         ChangeAnimation("RollDice");
         yield return new WaitForSecondsRealtime(1f);
