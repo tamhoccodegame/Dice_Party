1c1e9a21a67192c1869f1d526f74606bcd75d4db
Tam Doan <118838652+tamhoccodegame@users.noreply.github.com>
2025-08-24 23:20:59 +0700

    manager qua duong

---
+++ Assets/Project/Runtime/Scripts/QuaDuongManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaDuongManager : WizardMiniGameManager
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public override bool CheckGameOver()
+    {
+        return time <= 0;
+    }
+
+    public override void ShowGameOverPanel()
+    {
+        base.ShowGameOverPanel();
+    }
+
+    public override void SpawnRewardAvatar()
+    {
+        base.SpawnRewardAvatar();
+    }
+
+    protected override void TriggerAfterCutscene()
+    {
+        base.TriggerAfterCutscene();
+    }
+
+    public override void UpdateHUD()
+    {
+        base.UpdateHUD();
+    }
+
+}
