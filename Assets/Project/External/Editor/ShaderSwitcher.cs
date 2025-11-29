50c46951b0302b503f225d2b5fa69327141950e0
Tam Doan <118838652+tamhoccodegame@users.noreply.github.com>
2025-11-30 01:10:08 +0700

    Update Shader

---
+++ Assets/Project/External/Editor/ShaderSwitcher.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+public class ShaderSwitcher : Editor
+{
+    [MenuItem("Tools/Switch Shader of Selected Material(s) to NewToonShader")]
+    static void ChangeShader()
+    {
+        // Lấy material đang được chọn trong tab Project
+        Object[] selectedObjects = Selection.objects;
+
+        foreach (Object obj in selectedObjects)
+        {
+            Material mat = obj as Material;
+
+            if (mat == null)
+            {
+                Debug.LogWarning("Bạn phải chọn 1 Material trong Project!");
+                return;
+            }
+
+            Texture baseMap = null;
+
+            if (mat.HasProperty("_BaseMap"))
+                baseMap = mat.GetTexture("_BaseMap");
+
+            // Ví dụ đổi sang Standard Shader
+            Shader newShader = Shader.Find("Shader Graphs/NewToonShader");
+
+            if (newShader == null)
+            {
+                Debug.LogWarning("Không tìm thấy shader!");
+                return;
+            }
+
+            mat.shader = newShader;
+
+            if (mat.HasProperty("_BaseMap"))
+                mat.SetTexture("_BaseMap", baseMap);
+
+            Debug.Log("Đã đổi shader cho: " + mat.name);
+        }
+    }
+}
+
