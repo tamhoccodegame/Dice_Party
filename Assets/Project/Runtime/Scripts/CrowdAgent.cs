a7596aae9460205355ef6bccef4e498a3903098a
Tam Doan <118838652+tamhoccodegame@users.noreply.github.com>
2026-05-20 17:28:44 +0700

    Lastest

---
+++ Assets/Project/Runtime/Scripts/CrowdAgent.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CrowdAgent : MonoBehaviour
+{
+    public Transform player;
+
+    public float avoidRadius = 2f;
+    public float avoidForce = 40f;
+    public float returnSpeed = 20f;
+
+    Vector3 originalPos;
+    Vector3 velocity;
+
+    void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(transform.position, player.position);
+
+        if (dist < avoidRadius)
+        {
+            // Né player
+            Vector3 away = (transform.position - player.position).normalized;
+
+            float force = 1 - (dist / avoidRadius);
+
+            velocity += away * force * avoidForce * Time.deltaTime;
+        }
+        else
+        {
+            // Quay về vị trí cũ
+            Vector3 returnDir = (originalPos - transform.position);
+
+            velocity += returnDir * returnSpeed * Time.deltaTime;
+        }
+
+        transform.position += velocity * Time.deltaTime;
+
+        // làm mượt
+        velocity *= 0.9f;
+    }
+}
