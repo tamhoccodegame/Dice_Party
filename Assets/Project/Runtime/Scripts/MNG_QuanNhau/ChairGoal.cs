2b9b8ca09d67a4a0cc2b3464294b6e6522c812e7
Tam Doan <118838652+tamhoccodegame@users.noreply.github.com>
2026-06-07 22:34:57 +0700

    Update map

---
+++ Assets/Project/Runtime/Scripts/MNG_QuanNhau/ChairGoal.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairGoal : MonoBehaviour
+{
+    public Transform sitPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            other.GetComponent<TestMNGController>().enabled = false;
+            other.GetComponent<CharacterController>().enabled = false;
+            StartCoroutine(SmoothPositionPlayer(other.transform));
+        }
+    }
+
+    IEnumerator SmoothPositionPlayer(Transform player)
+    {
+        Animator animator = player.GetComponent<Animator>();
+        animator.CrossFade("Jump", 0.25f);
+
+
+        Vector3 startPos = player.position;
+        Vector3 endPos = sitPosition.position;
+
+        Quaternion startRot = player.rotation;
+        Quaternion endRot = sitPosition.rotation;
+
+        float duration = 0.3f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+
+            player.position = Vector3.Lerp(startPos, endPos, t);
+            player.rotation = Quaternion.Slerp(startRot, endRot, t);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        player.position = endPos;
+        player.rotation = endRot;
+
+        animator.CrossFade("Sitting", 0.25f);
+    }
+}
---
+++ Assets/Project/Runtime/Scripts/MNG_QuanNhau/MainTable.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTable : MonoBehaviour
+{
+    public Transform[] sitsPosition;
+    public int currentPosition = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            other.GetComponent<TestMNGController>().enabled = false;
+            other.GetComponent<CharacterController>().enabled = false;
+            StartCoroutine(SmoothPositionPlayer(other.transform));
+        }
+    }
+
+    IEnumerator SmoothPositionPlayer(Transform player)
+    {
+        Animator animator = player.GetComponent<Animator>();
+        animator.CrossFade("Jump", 0.25f);
+
+        Transform target = sitsPosition[currentPosition];
+
+        Vector3 startPos = player.position;
+        Vector3 endPos = target.position;
+
+        Quaternion startRot = player.rotation;
+        Quaternion endRot = target.rotation;
+
+        float duration = 0.5f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+
+            player.position = Vector3.Lerp(startPos, endPos, t);
+            player.rotation = Quaternion.Slerp(startRot, endRot, t);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        player.position = endPos;
+        player.rotation = endRot;
+
+        animator.CrossFade("Sitting", 0.25f);
+    }
+}
